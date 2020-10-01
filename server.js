5ba72da3258cc23cd40cf517d0d2c96dcf5a28ec
Archangel <nathapcal@gmail.com>
2020-09-30 20:21:36 -0400

    added server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var path = require("path");
+
+// PORTS
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// location of static files 
+app.use(express.static("public"));
+
+
+//Route
+app.get("/", function (req, res) {
+  res.sendFile(path.json(__dirname, "./public/assets/index.html"));
+});
+
+// Listener
+app.listen(PORT, function () {
+  console.log("App is listening on PORT: " + PORT);
+});
