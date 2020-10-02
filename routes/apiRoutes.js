6e88a38c6941a01eeb93f90a315510ce54259ff7
Archangel <nathapcal@gmail.com>
2020-10-01 23:11:11 -0400

    fixed the delete button.  emptied the database to function properly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -46,7 +46,6 @@ module.exports = function (app) {
 
   // Delete
   
-
   app.delete("/api/notes/:id", (req, res) => {
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
       if (err) {
@@ -57,10 +56,10 @@ module.exports = function (app) {
           message: "Unable to get note.",
         });
       }
-      const updatedData = JSON.parse(data).filter(function (data) {
+      const datainfo = JSON.parse(data)
+      updatedData = datainfo.filter(function (data) {
         return data.id != req.params.id;
       });
-      updatedData.push(req.body);
       console.log(updatedData);
       fs.writeFile("./db/db.json", JSON.stringify(updatedData), (err) => {
         if (err) {
